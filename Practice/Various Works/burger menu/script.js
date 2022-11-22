f4b9ab249676f947200bad85d9e028072403ed33
Nouleone <khueman.mp3@gmail.com>
2022-11-22 02:30:29 +0100

    Deleted

--- Practice/Various Works/burger menu/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.teset').click(function(event) {
-        $('.header__navigation-burger,.header__navigation-styles').toggleClass('active');
-        $('body').toggleClass('lock');
-        $('.header').toggleClass('bckgr')
-        $('.header__logo').toggleClass('bckgrlogo')
-    });
-});
